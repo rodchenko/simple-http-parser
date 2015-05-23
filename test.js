a18220a70c3543870fe1ca24cd2d3d5ede0736af
mmiisha <psyballs@gmail.com>
2015-05-23 19:31:33 +0300

    http parser works as a Method without parameters

--- test.js
+++ test.js
@@ -1,51 +1,65 @@
-// if (Meteor.isClient) {
-//   // counter starts at 0
-//   Session.setDefault('counter', 0);
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('text', '');
 
-//   Template.hello.helpers({
-//     counter: function () {
-//       return Session.get('counter');
-//     }
-//   });
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    },
+    text: function () {
+      return Session.get('text');
+    }
+  });
 
-//   Template.hello.events({
-//     'click button': function () {
-//       // increment the counter when button is clicked
-//       Session.set('counter', Session.get('counter') + 10);
-//     }
-//   });
-// }
+  Template.hello.events({
+    'click button': function () {
+      Meteor.call(
+        'parse', 
+        function(error, result) {
+          // console.log(result);
+          if(result != "error") {
+            Session.set('text', result);
+          }
+        }
+      );
+    }
+  });
+}
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-
-    HTTP.call(
-      "POST",
-      "http://www.500letters.org/form_15.php",
-      {
-        params: {
-          'f1_gender': 'male',
-          'f1_firstname': 'plus',
-          'f1_surname': 'minus',
-          'f1_birthyear': '2007',
-          'f1_birthCity': '',
-          'f1_country': '2',
-          'f1_workplaceCity': '',
-          'f4_theme': ['Abstraction'],
-          'generate_bio': 'Generate artist text',
-        },
-        headers: {
-          'Content-type': 'application/x-www-form-urlencoded'
+  //Meteor.startup(function () {
+  Meteor.methods({
+    parse: function() {
+      var result = HTTP.call(
+        "POST",
+        "http://www.500letters.org/form_15.php",
+        {
+          params: {
+            'f1_gender': 'male',
+            'f1_firstname': 'plus',
+            'f1_surname': 'minus',
+            'f1_birthyear': 2000+Math.floor(Math.random()*10),
+            'f1_birthCity': '',
+            'f1_country': '2',
+            'f1_workplaceCity': '',
+            'f4_theme': ['Abstraction'],
+            'generate_bio': 'Generate artist text',
+          },
+          headers: {
+            'Content-type': 'application/x-www-form-urlencoded'
+          }
         }
-      },
-      function (error, result) {
-        if(!error) {
-          // console.log(result.content);
-          var $ = cheerio.load(result.content);
-          console.log($('.text_container > .formdiv').text());
-        }
+      ); 
+      if(result) {
+        // console.log(result.content);
+        var $ = cheerio.load(result.content);
+        // console.log($('.text_container > .formdiv').text());
+        return $('.text_container > .formdiv').text();
       }
-    );
-
+      return "error";
+    }
   });
+
+
+  //});
 }
